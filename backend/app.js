c3967031d41baafe50423be4201297edc1bc56b5
itsabhi-21 <abhinavch2105@gmail.com>
2026-03-31 23:08:36 +0530

    connected

--- backend/app.js
+++ backend/app.js
@@ -19,11 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
     origin: [
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:3000',
-        'http://127.0.0.1:5173',
-        'http://127.0.0.1:5174',
+        process.env.FRONTEND_URL || 'http://localhost:5173',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
